6669d7ffbbd256c18218b87ed7ed789451369193
krishpranav <krisna.pranav@gmail.com>
2021-08-01 16:46:30 +0530

    lib: extend-http[ssl, connection]

--- lib/extend-http.rb
+++ lib/extend-http.rb
@@ -45,3 +45,86 @@ class ExtendedHTTP < Net::HTTP
 
 
     attr_reader :raw
+
+    def connect
+        @raw = []
+        if proxy?
+            conn_address = proxy_address
+            conn_port = proxy_port
+        else
+            conn_address = address
+            conn_port = port
+        end
+
+        D "opening connection to #{conn_address}:#{conn_port}..."
+        s = Timeout.timeout(@open_timeout, Net::OpenTimeout) do
+            TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
+        end
+
+        s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+        D 'opened'
+
+        if use_ssl?
+            ssl_parameters = {}
+            iv_list = instance_variables
+            SSL_IVNAMES.each_wit_index do |ivanme, i|
+                if iv_list.include?(ivname) &&
+                    (value = instance_variable_get(ivname))
+                ssl_parameters[SSL_ATTRIBUTES[i]] = value if value
+                end
+            end
+            @ssl_context = OpenSSL::SSL::SSLContext.new
+            @ssl_context.set_params(ssl_parameters)
+
+            D "starting SSL for #{conn_address}:#{conn_port}..."
+            s = OpenSSL::SSL::SSLSocket.new(s, @ssl_context)
+            s.sync_close = true
+            D 'SSL established'
+        end
+
+        @socket = BufferedIO.new(s)
+        @socket.read_timeout = @read_timeout
+        @socket.continue_timeout = @continue_timeout
+        @socket.debug_output = @debug_output
+        if use_ssl?
+            begin
+                if proxy?
+                    bug = "CONNECT #{@address}:#{@port} HTTP/#{HTTPVersion}\r\n"
+                    buf << "Host: #{@address}:#{@port}\r\n"
+
+                    if proxy_user
+                        credential = ["#{proxy_user}:#{proxy_pass}"].pack('m')
+                        credential.delete!("\r\n")
+                        buf << "Proxy-Authorization: Basic #{credential}\r\n"
+                    end
+
+                    buf << "\r\n"
+                    @socket.write(buf)
+                    _x, raw = ExtendedHTTPResponse.read_new(@socket)
+                    @raw << raw
+                end
+
+                if @ssl_session && 
+                    Process.clock_gettime(Process::CLOCK_REALTIME) < @ssl_session.time.to_f + @ssl_session.timeout
+                    s.session = @ssl_session if @ssl_session
+                end
+
+                s.hostname = @address if s.respond_to? :hostname=
+                Timeout.timeout(@open_timeout, Net::OpenTimeout) { s.connect }
+                if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
+                    s.post_connection_check(@address)
+                end
+
+                @ssl_session = s.session
+            rescue => exception
+                D "conn closeed"
+                @socket.close if @socket && !socket.closed?
+                raise exception
+            end
+        end
+
+        on_connect
+    end
+
+    private :connect
+            
