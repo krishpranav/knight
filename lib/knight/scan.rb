8e1fac2b0b336e684eb9cac7bf07f76c1a5f534b
krishpranav <krisna.pranav@gmail.com>
2021-08-01 18:56:03 +0530

    lib: knight scan

--- lib/knight/scan.rb
+++ lib/knight/scan.rb
@@ -22,7 +22,6 @@ module WhatWeb
 
       workers = (1..@max_threads).map do
         Thread.new do
-
           loop do
             target = @target_queue.pop
             Thread.exit unless target
@@ -40,13 +39,11 @@ module WhatWeb
         end
       end
 
-      # initialize target_queue
       @targets.each do |url|
         target = prepare_target(url)
         next unless target
         @target_queue << target
       end
-
 
       loop do
 
@@ -70,6 +67,7 @@ module WhatWeb
     end
 
     def add_target(url, redirect_counter = 0)
+
       target = Target.new(url, redirect_counter)
 
       unless target
@@ -83,9 +81,90 @@ module WhatWeb
     private
 
     def prepare_target(url)
-    	Target.new(url)
+      Target.new(url)
     rescue => e
-    	error("Prepare Target Failed - #{e}")
-    	nil
+      error("Prepare Target Failed - #{e}")
+      nil
     end
 
+    def make_target_list(urls, opts = {})
+      url_list = []
+
+      if urls.is_a?(Array)
+        urls.flatten.reject { |u| u.nil? }.map { |u| u.strip }.reject { |u| u.eql?('') }.each do |url|
+          url_list << url
+        end
+      end
+
+      inputfile = opts[:input_file] || nil
+      if !inputfile.nil? && File.exist?(inputfile)
+        pp "loading input file: #{inputfile}" if $verbose > 2
+        File.open(inputfile).readlines.each(&:strip!).reject { |line| line.start_with?('#') || line.eql?('') }.each do |line|
+          url_list << line
+        end
+      end
+
+      return [] if url_list.empty?
+
+      ip_range = url_list.map do |x|
+        range = nil
+        if x =~ %r{^[0-9\.\-\/]+$} && x !~ %r{^[\d\.]+$}
+          begin
+
+            if x =~ %r{\d+\.\d+\.\d+\.\d+/\d+$}
+              range = IPAddr.new(x).to_range.map(&:to_s)
+            elsif x =~ %r{^(\d+\.\d+\.\d+\.\d+)-(\d+)$}
+              start_ip = IPAddr.new(Regexp.last_match(1), Socket::AF_INET)
+              end_ip   = IPAddr.new("#{start_ip.to_s.split('.')[0..2].join('.')}.#{Regexp.last_match(2)}", Socket::AF_INET)
+              range = (start_ip..end_ip).map(&:to_s)
+
+            elsif x =~ %r{^(\d+\.\d+\.\d+\.\d+)-(\d+\.\d+\.\d+\.\d+)$}
+              start_ip = IPAddr.new(Regexp.last_match(1), Socket::AF_INET)
+              end_ip   = IPAddr.new(Regexp.last_match(2), Socket::AF_INET)
+              range = (start_ip..end_ip).map(&:to_s)
+            end
+          rescue => e
+
+            raise "Error parsing target IP range: #{e}"
+          end
+        end
+        range
+      end.compact.flatten
+
+      url_list = url_list.select { |x| !(x =~ %r{^[0-9\.\-*\/]+$}) || x =~ /^[\d\.]+$/ }
+      url_list += ip_range unless ip_range.empty?
+
+      push_to_urllist = []
+
+      url_list = url_list.map do |x|
+        if File.exist?(x)
+          x
+        else
+          x = opts[:url_pattern].gsub('%insert%', x) unless opts[:url_pattern].to_s.eql?('')
+          x = "#{opts[:url_prefix]}#{x}#{opts[:url_suffix]}"
+
+          if x !~ %r{^[a-z]+:\/\/}
+
+            x.sub!(/^/, 'http://')
+          end
+
+          begin
+            domain = Addressable::URI.parse(x)
+            raise 'Unable to parse invalid target. No hostname.' if domain.host.empty?
+
+            x = domain.normalize.to_s if domain.host !~ %r{^[a-zA-Z0-9\.:\/]*$}
+          rescue => e
+
+            x = nil
+            error("Unable to parse invalid target #{x}: #{e}")
+          end
+          x
+        end
+      end
+
+      url_list += push_to_urllist unless push_to_urllist.empty?
+
+      url_list = url_list.flatten.compact 
+    end
+  end
+end
